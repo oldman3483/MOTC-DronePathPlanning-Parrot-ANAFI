dd594ab51771732d520cc114077bc54877c6fd66
oldman3483 <qazws3483@gmail.com>
2021-08-04 14:50:09 +0800

    faster Single Flight mode

---
+++ SinglePtFlight_mod.py
@@ -0,0 +1,143 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun July 9 12:01:31 2021
+
+@author: ZXLi
+"""
+
+from utils import readPoints, mapGrid, mapGrid4demo, outpathIMG, IsObstalce, readmap
+from astar_forUse import next_move
+from math  import sqrt, sin, cos
+import time
+
+
+
+def singlePointFlight(fname_pts:str, fname_start:str):
+    start_time = time.time()
+    inPts = readPoints(fname_pts)
+    startPts = readPoints(fname_start)
+    radius = 20  # need to be an input from reading file 
+    grid = mapGrid()
+
+    xin = []
+    yin = []
+
+#    xin+=l_xstartPts
+#    yin+=l_ystartPts
+    # x coordinate
+
+    print("-------------------------------------------")
+
+    for i in range(0, 360, 2):
+        xtmp = int(inPts[0]+cos(90-i)*radius)
+        ytmp = int(inPts[1]+sin(90-i)*radius)
+
+        xin.append(xtmp)
+        yin.append(ytmp)
+    
+        print("xin: "+str(xin[int(i/2)]))
+        print("yin: "+str(yin[int(i/2)]))
+    print("-------------------------------------------")
+
+    # inputpts = checkPts(xin, yin)
+    distance = []
+
+    intpts_r = len(xin)
+    for r in range(int(intpts_r)):
+        calc = sqrt((xin[0]-xin[r])**2+(yin[0]-yin[r])**2)
+        #print("calc = " +str(calc))
+        #print(distance)
+        distance.append(calc)
+    
+    sorted_distance = sorted(distance)
+    print("DIS = "+str(distance))
+    print(sorted_distance)
+    
+    start_index = 0
+
+    for i in range(len(sorted_distance)):
+        calc = sqrt((xin[0]-xin[i])**2+(yin[0]-yin[i])**2)
+        if calc == sorted_distance[0]:
+            start_index = i
+    print(start_index)
+    grid = mapGrid()
+    
+    xin_sorted = []
+    yin_sorted = []
+    
+    index = start_index
+    for i in range(len(sorted_distance)):
+        if index == len(sorted_distance):
+            index = 0
+        xin_sorted.append(xin[index])
+        yin_sorted.append(yin[index])
+
+        index+=1
+    l_xstartPts = startPts[0].tolist()
+    l_ystartPts = startPts[1].tolist()
+    xin_sorted.append(xin[start_index])
+    yin_sorted.append(yin[start_index])
+
+    print(xin_sorted)
+
+    print("-------------------------------------------")
+    xPts = l_xstartPts+[xin_sorted[1]]
+    yPts = l_ystartPts+[yin_sorted[1]]
+    
+    print("-------------------------------------------")
+    print(xPts, yPts)
+    print("-------------------------------------------")
+    print(type(xPts))
+
+    pts_pathNum = []
+    xpts_planPath = []
+    ypts_planPath = []
+    for i in range(1):
+        print("Start X: "+str(xPts[i]))
+        print("Start Y: "+str(yPts[i]))
+
+        (pathNum, planPath) = next_move((xPts[i], yPts[i]),(xPts[i+1], yPts[i+1]), grid.tolist(), "singlePts_5buff_100m_test.txt")
+        pts_pathNum.append(pathNum)
+        xpts_planPath+=planPath[0]
+        ypts_planPath+=planPath[1]
+        print("End X: "+str(xPts[i+1]))
+        print("End Y: "+str(yPts[i+1]))
+        print("\n")
+    end_time = time.time()
+    print(pts_pathNum)
+    print(xpts_planPath)
+    print(len(xpts_planPath))
+    xpts_planPath_back = list(reversed(xpts_planPath))
+    ypts_planPath_back = list(reversed(ypts_planPath))
+    xpts_planPath += xin + xpts_planPath_back
+    ypts_planPath += yin +ypts_planPath_back
+    #print(pts_pathNum)
+    print("-------------------------------------------")
+    print(xpts_planPath)
+    print("-------------------------------------------")
+
+    print("time is =="+str(end_time-start_time))
+    respath = []
+    respath.append(xpts_planPath)
+    respath.append(ypts_planPath)
+    print("-------------------------------------------")
+
+    print(respath)
+    print("-------------------------------------------")
+
+    grid4demo = mapGrid4demo()
+    outpathIMG(grid,respath, xPts, yPts, "singlePts_5buff_75m_ang2_ori")
+    outpathIMG(grid4demo,respath, xPts, yPts, "singlePts_5buff_ang2_75m")
+    
+
+
+def checkPts(xlist:list, ylist:list) -> list:
+    # check whether each point is over the obstacle height setting 
+    # return the indexes list which are over the height
+    
+    res_pts = []
+
+    return res_pts
+
+
+
